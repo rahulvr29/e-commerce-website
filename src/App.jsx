caa92c6aa17a7a83ed656177e3daa9a414be99cf
RAHUL VR <iamrahulvr29@gmail.com>
2023-11-27 22:52:07 +0530

    create vite+react app project - Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="">
+        hello world
+      </div>
+    </>
+  )
+}
+
+export default App
